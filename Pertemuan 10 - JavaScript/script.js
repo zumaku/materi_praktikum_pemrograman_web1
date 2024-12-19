63d0688bfb97616badb2c7e4ccdb7a844f61b086
zumaku <60200121101@uin-alauddin.ac.id>
2024-12-19 14:39:05 +0800

    Last Commit

--- Pertemuan 10 - JavaScript/script.js
+++ Pertemuan 10 - JavaScript/script.js
@@ -5,6 +5,7 @@ const btn2 = document.querySelector(".btn2")
 btn1.addEventListener("click", () => {
     h1.classList.add("blue")
 })
+
 btn2.addEventListener("click", () => {
     h1.classList.add("green")
 })
